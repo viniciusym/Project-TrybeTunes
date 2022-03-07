a090cbac6a23e4fca814739f24dc7544b0589ee3
viniciusyamate <viniciusyamate5@gmail.com>
2022-03-07 20:05:59 +0000

    requisito 4 concluido

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -29,6 +30,9 @@ class Header extends React.Component {
     return loading ? <Loading /> : (
       <header data-testid="header-component">
         <p data-testid="header-user-name">{ name }</p>
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
       </header>
     );
   }
