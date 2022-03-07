44bf98ed158561ff1f47d0270306e0e49a94a710
viniciusyamate <viniciusyamate5@gmail.com>
2022-03-07 18:04:25 +0000

    requisito 1: Album component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
+import Album from './pages/Album';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
+          <Route path="/album/:id" render={ () => <Album /> } />
           <Route path="/search" render={ () => <Search /> } />
           <Route path="/" render={ () => <Login /> } />
         </Switch>
---
+++ src/pages/Album.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Album extends React.Component {
+  render() {
+    return (
+      <div className="page-album" data-testid="page-album">
+        Album
+      </div>
+    );
+  }
+}
+
+export default Album;
