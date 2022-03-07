6cfea96dbb7f8c6f170273ce02acc9dd89c4e163
viniciusyamate <viniciusyamate5@gmail.com>
2022-03-07 17:59:52 +0000

    requisito 1: Search component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
+          <Route path="/search" render={ () => <Search /> } />
           <Route path="/" render={ () => <Login /> } />
         </Switch>
       </BrowserRouter>
