b4c36a55664298d7b309dea15bf6ba901849d765
viniciusyamate <viniciusyamate5@gmail.com>
2022-03-07 18:15:52 +0000

    requisito 1: ProfileEdit component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Search from './pages/Search';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
 
 class App extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route path="/album/:id" render={ () => <Album /> } />
+          <Route path="/profile/edit" render={ () => <ProfileEdit /> } />
           <Route path="/profile" render={ () => <Profile /> } />
           <Route path="/favorites" render={ () => <Favorites /> } />
           <Route path="/search" render={ () => <Search /> } />
---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProfileEdit extends React.Component {
+  render() {
+    return (
+      <div className="page-profile-edit" data-testid="page-profile-edit">
+        Profile Edit
+      </div>
+    );
+  }
+}
+
+export default ProfileEdit;
