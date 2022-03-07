42df5f820e9c47c8a47f82a19892033a6964c6b4
viniciusyamate <viniciusyamate5@gmail.com>
2022-03-07 17:54:20 +0000

    requisito 1: Login component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" render={ () => <Login /> } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div className="page-login" data-testid="page-login">
+        Login
+      </div>
+    );
+  }
+}
+export default Login;
