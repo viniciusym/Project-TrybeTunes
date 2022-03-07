39faee2fb4c46f725520b2447e4940c08eba3273
viniciusyamate <viniciusyamate5@gmail.com>
2022-03-07 18:08:07 +0000

    requisito 1: Favorites component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
+import Favorites from './pages/Favorites';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route path="/album/:id" render={ () => <Album /> } />
+          <Route path="/favorites" render={ () => <Favorites /> } />
           <Route path="/search" render={ () => <Search /> } />
           <Route path="/" render={ () => <Login /> } />
         </Switch>
