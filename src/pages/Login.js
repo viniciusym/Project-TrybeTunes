4dd802456712b9603dc44ca1af9054f2c49ec81f
viniciusyamate <viniciusyamate5@gmail.com>
2022-03-07 18:43:39 +0000

    requisito 2: cria input para username e cria botao para login com validaçao

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,48 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value, name } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { name } = this.state;
+    const minNameLength = 3;
     return (
       <div className="page-login" data-testid="page-login">
-        Login
+        <form>
+          <label htmlFor="name">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              onChange={ this.handleChange }
+              data-testid="login-name-input"
+            />
+          </label>
+          <label htmlFor="login">
+            <input
+              disabled={ name.length < minNameLength }
+              type="button"
+              value="Entrar"
+              id="login"
+              name="login"
+              data-testid="login-submit-button"
+            />
+          </label>
+        </form>
       </div>
     );
   }
